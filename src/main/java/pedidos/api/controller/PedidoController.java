6ad1ac5151a2d4f0793d531bddf2a165e503af9a
marcelo <marceloviana1991@gmail.com>
2024-10-25 22:11:51 -0300

    Implementando melhorias de desing de codigo

--- src/main/java/pedidos/api/controller/PedidoController.java
+++ src/main/java/pedidos/api/controller/PedidoController.java
@@ -1,6 +1,5 @@
 package pedidos.api.controller;
 
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-import pedidos.api.dto.pedido.DadosCadastroItem;
 import pedidos.api.dto.pedido.DadosCadastroPedido;
 import pedidos.api.dto.pedido.DadosDetalhamentoItem;
 import pedidos.api.dto.pedido.DadosDetalhamentoPedido;
@@ -19,16 +17,12 @@ import pedidos.api.service.pedido.PedidoService;
 import pedidos.api.model.*;
 import pedidos.api.repository.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @RestController
 @RequestMapping("/pedidos")
 public class PedidoController {
-
-    @Autowired
-    private ItemRepository itemRepository;
 
     @Autowired
     private PedidoRepository pedidoRepository;
@@ -44,31 +38,17 @@ public class PedidoController {
         Usuario usuario = pedidoService.capturarUsuarioLogado(request);
         Pedido pedido = new Pedido(usuario);
         pedidoRepository.save(pedido);
-        List<DadosDetalhamentoItem> dadosDetalhamentoItemList = new ArrayList<>();
-        for (DadosCadastroItem dadosCadastroItem: dadosCadastroPedido.itens()) {
-            Produto produto = pedidoService.retirarProdutoEmEstoque(dadosCadastroItem.idProduto(),
-                    dadosCadastroItem.quantidade());
-            Item item = new Item(dadosCadastroItem,pedido,produto);
-            itemRepository.save(item);
-            DadosDetalhamentoItem dadosDetalhamentoItem = new DadosDetalhamentoItem(item);
-            dadosDetalhamentoItemList.add(dadosDetalhamentoItem);
-        }
+        List<DadosDetalhamentoItem> dadosDetalhamentoItemList = pedidoService.adicionarItensAoPedido(
+                dadosCadastroPedido.itens(),pedido, pedidoService);
         var uri = uriComponentsBuilder.path("/pedidos/{id}").buildAndExpand(pedido.getId()).toUri();
         return ResponseEntity.created(uri).body(new DadosDetalhamentoPedido(pedido, dadosDetalhamentoItemList));
     }
 
     @GetMapping
     public ResponseEntity<List<DadosDetalhamentoPedido>> listar(Pageable pageable) {
-        List<DadosDetalhamentoPedido> dadosDetalhamentoPedidoList = new ArrayList<>();
         Page<Pedido> pedidoList = pedidoRepository.findAll(pageable);
-        for (Pedido pedido: pedidoList) {
-            List<Item> itemList = itemRepository.findAllByPedido(pedido);
-            List<DadosDetalhamentoItem> dadosDetalhamentoItemList = new ArrayList<>();
-            for (Item item: itemList) {
-                dadosDetalhamentoItemList.add(new DadosDetalhamentoItem(item));
-            }
-            dadosDetalhamentoPedidoList.add(new DadosDetalhamentoPedido(pedido, dadosDetalhamentoItemList));
-        }
+        List<DadosDetalhamentoPedido> dadosDetalhamentoPedidoList = pedidoService.capturarItensDeListaDePedidos(
+                pedidoList);
         return ResponseEntity.ok(dadosDetalhamentoPedidoList);
     }
 
@@ -79,8 +59,7 @@ public class PedidoController {
         if (!Objects.equals(usuario, pedido.getUsuario()) && usuario.getTipoUsuario() == TipoUsuario.USER) {
             throw new ValidacaoException("Esse pedido não pertence ao usuário logado!");
         }
-        List<DadosDetalhamentoItem> dadosDetalhamentoItemList = itemRepository.findAllByPedido(pedido).stream()
-                .map(DadosDetalhamentoItem::new).toList();
+        List<DadosDetalhamentoItem> dadosDetalhamentoItemList = pedidoService.capturarItensDePedido(pedido);
         return ResponseEntity.ok(new DadosDetalhamentoPedido(pedido, dadosDetalhamentoItemList));
     }
 
@@ -88,7 +67,7 @@ public class PedidoController {
     @Transactional
     public ResponseEntity<?> excluir(@PathVariable Long id) {
         Pedido pedido = pedidoRepository.getReferenceById(id);
-        itemRepository.deleteAllByPedido(pedido);
+        pedidoService.excluirItensDePedido(pedido);
         pedidoRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/pedidos/api/service/pedido/PedidoService.java
+++ src/main/java/pedidos/api/service/pedido/PedidoService.java
@@ -2,13 +2,23 @@ package pedidos.api.service.pedido;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import pedidos.api.dto.pedido.DadosCadastroItem;
+import pedidos.api.dto.pedido.DadosDetalhamentoItem;
+import pedidos.api.dto.pedido.DadosDetalhamentoPedido;
 import pedidos.api.infra.exception.ValidacaoException;
+import pedidos.api.model.Item;
+import pedidos.api.model.Pedido;
 import pedidos.api.model.Produto;
 import pedidos.api.model.Usuario;
+import pedidos.api.repository.ItemRepository;
 import pedidos.api.repository.ProdutoRepository;
 import pedidos.api.repository.UsuarioRepository;
 import pedidos.api.service.security.TokenService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PedidoService {
@@ -21,6 +31,10 @@ public class PedidoService {
 
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
 
     public Usuario capturarUsuarioLogado(HttpServletRequest request) {
         var tokenJWT = tokenService.recuperarToken(request);
@@ -35,5 +49,41 @@ public class PedidoService {
         }
         produto.retirarEmEstoque(quantidadeRetirada);
         return produto;
+    }
+
+    public List<DadosDetalhamentoItem> adicionarItensAoPedido(
+            List<DadosCadastroItem> itens, Pedido pedido, PedidoService pedidoService) {
+        List<DadosDetalhamentoItem> dadosDetalhamentoItemList = new ArrayList<>();
+        for (DadosCadastroItem dadosCadastroItem: itens) {
+            Produto produto = pedidoService.retirarProdutoEmEstoque(dadosCadastroItem.idProduto(),
+                    dadosCadastroItem.quantidade());
+            Item item = new Item(dadosCadastroItem,pedido,produto);
+            itemRepository.save(item);
+            DadosDetalhamentoItem dadosDetalhamentoItem = new DadosDetalhamentoItem(item);
+            dadosDetalhamentoItemList.add(dadosDetalhamentoItem);
+        }
+        return dadosDetalhamentoItemList;
+    }
+
+    public List<DadosDetalhamentoPedido> capturarItensDeListaDePedidos(Page<Pedido> pedidoList) {
+        List<DadosDetalhamentoPedido> dadosDetalhamentoPedidoList = new ArrayList<>();
+        for (Pedido pedido: pedidoList) {
+            List<Item> itemList = itemRepository.findAllByPedido(pedido);
+            List<DadosDetalhamentoItem> dadosDetalhamentoItemList = new ArrayList<>();
+            for (Item item: itemList) {
+                dadosDetalhamentoItemList.add(new DadosDetalhamentoItem(item));
+            }
+            dadosDetalhamentoPedidoList.add(new DadosDetalhamentoPedido(pedido, dadosDetalhamentoItemList));
+        }
+        return dadosDetalhamentoPedidoList;
+    }
+
+    public List<DadosDetalhamentoItem> capturarItensDePedido(Pedido pedido) {
+        return itemRepository.findAllByPedido(pedido).stream()
+                .map(DadosDetalhamentoItem::new).toList();
+    }
+
+    public void excluirItensDePedido(Pedido pedido) {
+        itemRepository.deleteAllByPedido(pedido);
     }
 }
